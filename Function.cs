94ec5628c46a9e6dd8b0baeb6190975b1daa42dd
Luis Gerardo Rodriguez Garcia <78064083+GerardoRdz96@users.noreply.github.com>
2024-03-22 00:30:14 -0600

    Add files via upload

--- Function.cs
+++ Function.cs
@@ -1,23 +1,29 @@
 using Amazon.Lambda.Core;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
-[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
 
 namespace NotificadorMain;
 
 public class Function
 {
-    
+
     /// <summary>
     /// A simple function that takes a string and returns both the upper and lower case version of the string.
     /// </summary>
-    /// <param name="input">The event for the Lambda function handler to process.</param>
-    /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
+    /// <param name="input"> The event for the Lambda function handler to process.</param>
+    /// <param name="context"> The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
     /// <returns></returns>
-    public Casing FunctionHandler(string input, ILambdaContext context)
+
+    [LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+    public string FunctionHandler(MyData data, ILambdaContext context)
     {
-        return new Casing(input.ToLower(), input.ToUpper());
+        return $"Hola {data.Name}".ToUpper();
     }
 }
 
-public record Casing(string Lower, string Upper);
+public class MyData
+{
+    public string Name { get; set; }
+}
